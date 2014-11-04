8d9b8fceb23de1830ae8f70a69a3c2cc98abf6be
Malte Ahrnes <MalteAhrens@arcor.de>
2014-11-04 23:31:11 +0100

    next iteration

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,9 @@
  */
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
-
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
   });
