e497a593dc60b07f4efad8ddd7d9666daecf9d71
Malte Ahrnes <MalteAhrens@arcor.de>
2014-11-04 19:56:16 +0100

    added name

--- Gruntfile.js
+++ Gruntfile.js
@@ -308,7 +308,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'CNAME'
           ]
         }, {
           expand: true,
