54e04271893a48bf2299ee484c639f330b65fabc
Malteser <MalteAhrens@arcor.de>
2014-11-04 11:22:11 +0100

    added _config.yml to gh-pages branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,6 +292,7 @@ module.exports = function (grunt) {
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*',
 			'CNAME',
+			'_config.yml'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
