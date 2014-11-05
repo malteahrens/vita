1af2e732470a09153c40d24b594253c55923c5b1
Malte Ahrnes <MalteAhrens@arcor.de>
2014-11-05 20:16:20 +0100

    test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-     'pascalprecht.translate'
+    'pascalprecht.translate'
   ])
   .config(function ($routeProvider, $translateProvider) {
     $routeProvider
