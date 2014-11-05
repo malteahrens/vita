340bf3dbcfef0338ec5962b43428ad724dc2d7e1
Malteser <MalteAhrens@arcor.de>
2014-11-05 16:29:33 +0100

    test

--- app/scripts/directives/mouseover.js
+++ app/scripts/directives/mouseover.js
@@ -7,23 +7,20 @@
  * If there is a value attached to the directive this will be the id for the mouseover action, otherwise it will try to read the id from the translation directive (if present)
  */
 angular.module('angularApp')
-  .directive('mouseover', function () {
+  .directive('mouseover', function ($parse) {
     return {
-        link: function (scope, iElement, attrs) {		
-            iElement.bind('mouseover', function ($event) {
-				var attribute = attrs.mouseover;
-				
-				if(attribute == "" && $event.target.attributes['translate'] != undefined) {
-					// there was no value set for mouseover attr - try to get an id from translation directive
-					console.log("try to set from translate directive");
-					attribute = $event.target.attributes['translate'].value;
-					console.log(attribute);
-				}
-				
+		restrict: 'EA',
+		controller: function ($scope) {
+			$scope.imgSrc = {}
+		},
+        link: function ($scope, element, attrs) {		
+            element.bind('mouseover', function ($event) {
+				var attribute = attrs.mouseover;	
 				var source = "images/"+attribute+".png";
 				console.log("set image source to "+ source);
-				scope.imgSrc = source;
-				scope.$apply();
+				$scope.$apply(function() {
+					$scope.imgSrc = source;
+				});
             });
         }
     };
