05ea34044e6a1ac6acc5ef0dd510f3c33dd4d152
Malte Ahrnes <MalteAhrens@arcor.de>
2014-11-05 20:42:21 +0100

    test fix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-translate/angular-translate.js',
-      'bower_components/angular-translate/angular-translate-storage-cookie.js',
+      'bower_components/angular-translate-storage-cookie/angular-translate-storage-cookie.js',
       'app/scripts/**/*.js',
       'test/spec/**/*.js'
     ],
