0b35ccde38bae16fd2f3901583505542d7f8cbb1
Malte Ahrnes <MalteAhrens@arcor.de>
2014-11-07 16:57:18 +0100

    test

--- app/scripts/directives/mouseover.js
+++ app/scripts/directives/mouseover.js
@@ -12,17 +12,58 @@ angular.module('angularApp')
 		restrict: 'EA',
 		controller: function ($scope) {
 			$scope.imgSrc = "images/yeoman.png";
-			$scope.hideSwitch = false;
+            $scope.fadeIn = true;
 		},
         link: function ($scope, element, attrs) {
-            element.bind('mouseover', function ($event) {
+            element.bind('mouseenter', function ($event) {
 				$scope.$apply(function() {
-					$scope.hideSwitch = true;
-					// start animation to make the image visible...
-					//$scope.imgSrc = attrs.mouseover;
+					$scope.imgSrc = attrs.mouseover;
 				});
+            });
+
+            element.bind('mouseleave', function ($event) {
+                $scope.$apply(function() {
+                    $scope.fadeIn = false;
+                });
             });
         }
     };
 	
   });
+
+angular.module('angularApp')
+    .directive('fadeIn', function ($animate) {
+        return {
+            restrict: 'A',
+            scope: {
+                ngSrc: '@',
+                fadeIn: '@'
+            },
+            link: function(scope, element, attrs) {
+
+                element.on('load', function() {
+                    console.log("load");
+                }).on('error', function() {
+                    console.log("error loading image");
+                });
+
+                scope.$watch('ngSrc', function(newVal) {
+                    console.clear();
+                    console.log("change");
+                    // trigger animation and wait until it is finshed... then reset
+                    $animate.removeClass(element, 'fadein');
+                        var promise = $animate.addClass(element, 'fadein');
+                        console.log(element);
+                        promise.then(function () {
+                            // if we are in here, the animation is complete
+                            console.log("animation finished");
+                            console.log(element);
+                            $animate.removeClass(element, 'fadein');
+                            console.log(element);
+                        });
+
+                });
+            }
+        };
+
+    });
