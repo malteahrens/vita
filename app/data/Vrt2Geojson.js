8843a9730f14d9826be4f46b147931736fcad6d9
Malte Ahrnes <MalteAhrens@arcor.de>
2015-03-10 00:24:16 +0100

    test

--- app/data/Vrt2Geojson.js
+++ app/data/Vrt2Geojson.js
@@ -1,2 +1,10 @@
 var gdal = require("gdal");
 console.dir(gdal.version)
+
+var dataset = gdal.open("PasingWlan.vrt");
+var layer = dataset.layers.get(0);
+
+console.log("number of features: " + layer.features.count());
+console.log("fields: " + layer.fields.getNames());
+console.log("extent: " + JSON.stringify(layer.extent));
+console.log("srs: " + (layer.srs ? layer.srs.toWKT() : 'null'));
