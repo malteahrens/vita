270443e9aaf8eb887ab3ec0d181dbd2b1da008f9
Malte Ahrnes <MalteAhrens@arcor.de>
2015-03-10 18:19:02 +0100

    test

--- app/data/Vrt2Geojson.js
+++ app/data/Vrt2Geojson.js
@@ -2,4 +2,11 @@ var gdal = require("gdal");
 // https://www.npmjs.com/package/ogr2ogr
 var ogr2ogr = require('ogr2ogr');
 
-console.dir(gdal.version)
+console.dir(gdal.version);
+var ogr = ogr2ogr('Stadtbezirk21.shp');
+
+ogr.exec(function (er, data) {
+    if (er) console.error(er)
+    console.log(data)
+})
+
