5e12070dc6f851d42e9d28afb3b40cf38325f03a
Malte Ahrnes <MalteAhrens@arcor.de>
2015-03-12 17:37:10 +0100

    test

--- deploy/scripts/Geojson2VectorTiles.js
+++ deploy/scripts/Geojson2VectorTiles.js
@@ -1,4 +1,3 @@
-var tilelive = require('tilelive');
-require('tilelive-bridge').registerProtocols(tilelive);
+var mapnik = require('mapnik');
 
 console.log("This script generates vector tiles from a GeoJSON file...")
