89c8364bb22e8a58d5238be9c19343b3b7c371f2
Malte Ahrnes <MalteAhrens@arcor.de>
2015-03-12 13:57:07 +0100

    tewt

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -44,7 +44,7 @@ angular.module('angularApp')
                    map: {
                        fullscreenControl: true
                    }
-               }
+               },
                layers: {
                    overlays: {
                        heatmap: {
