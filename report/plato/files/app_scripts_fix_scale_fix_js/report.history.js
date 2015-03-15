9ac78505b47565b0a7c4ab7efbd8fb0d329fe093
deploy.sh <xyz@test.de>
2015-03-15 11:39:33 +0000

    publish: test
    generated from commit 64fbb2d40381cbd34bfd898d8ef7dff295b7a515

--- report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
+++ report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 15 Mar 2015 11:28:30 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
+__history = [{"date":"Sun, 15 Mar 2015 11:38:38 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
