a375c123b84c5f63895a3d83775752057fb29519
Malte Ahrnes <MalteAhrens@arcor.de>
2015-03-28 13:14:52 +0100

    test

--- deploy/scripts/sqlite.js
+++ deploy/scripts/sqlite.js
@@ -1,10 +1,23 @@
+var fs = require("fs");
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('/app/data/wigle/PasingWlan.sqlite');
 
-db.serialize(function() {
-    db.get("SELECT * FROM location", function(err, row) {
-        console.log(row);
-    });
-});
+var file = "./app/data/PasingWlan.sqlite";
+var exists = fs.existsSync(file);
+var geojson = new Array();
 
-db.close();
+if(exists) {
+    var db = new sqlite3.Database(file);
+        db.each("SELECT * FROM location", function(err, row) {
+            geojson.push(row);
+        }, function(err, row) {
+            for(var i=0; i<geojson.length; i++) {
+                if(geojson[i]._id == 59182) {
+                    console.log("hit");
+                }
+            }
+        });
+
+    db.close();
+} else {
+    console.log("database not found...");
+}
