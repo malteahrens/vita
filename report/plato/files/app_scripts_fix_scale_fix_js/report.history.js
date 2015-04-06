a4191d6b47929e384ed3e7297e9df702b929ff55
deploy.sh <xyz@test.de>
2015-04-06 19:29:27 +0000

    publish: test
    generated from commit bc7bc268dc51e76ac1c8562b4f52641f97803e09

--- report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
+++ report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 06 Apr 2015 19:16:44 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
+__history = [{"date":"Mon, 06 Apr 2015 19:27:38 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 06 Apr 2015 19:16:54 GMT","total":{"sloc":21197,"maintainability":972.4439057309331},"average":{"sloc":1630,"maintainability":"74.80"}}]
+__history = [{"date":"Mon, 06 Apr 2015 19:27:49 GMT","total":{"sloc":21197,"maintainability":972.4439057309331},"average":{"sloc":1630,"maintainability":"74.80"}}]
