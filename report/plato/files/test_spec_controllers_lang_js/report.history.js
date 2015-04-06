dcc159a19b287ed9d5a8ff43330714194f980f6d
deploy.sh <xyz@test.de>
2015-04-06 17:30:04 +0000

    publish: test
    generated from commit f175a07ad2b15bc525ed95698e4662efb2588ddc

--- report/plato/files/test_spec_controllers_lang_js/report.history.js
+++ report/plato/files/test_spec_controllers_lang_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 06 Apr 2015 17:27:21 GMT","sloc":22,"lloc":11,"functions":3,"deliveredBugs":0.08744365791229632,"maintainability":79.87329168300973,"lintErrors":8,"difficulty":5.090909090909091}]
+__history = [{"date":"Mon, 06 Apr 2015 17:28:14 GMT","sloc":22,"lloc":11,"functions":3,"deliveredBugs":0.08744365791229632,"maintainability":79.87329168300973,"lintErrors":8,"difficulty":5.090909090909091}]
