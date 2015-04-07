9434a5c77a06ea49adf86dc0109b1dba569e42c4
deploy.sh <xyz@test.de>
2015-04-07 08:55:44 +0000

    publish: test
    generated from commit 68b53d9531a2165eb6aff17f0faee9bbdf9f927f

--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 07 Apr 2015 08:23:03 GMT","total":{"sloc":21197,"maintainability":972.4439057309331},"average":{"sloc":1630,"maintainability":"74.80"}}]
+__history = [{"date":"Tue, 07 Apr 2015 08:53:40 GMT","total":{"sloc":21197,"maintainability":972.4439057309331},"average":{"sloc":1630,"maintainability":"74.80"}}]
