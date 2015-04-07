b2c843e533c82013037976578ae275e9085d7bc3
deploy.sh <xyz@test.de>
2015-04-07 07:11:55 +0000

    publish: clipping for geojson
    generated from commit 0a31b224ac7730c44df410854de157e0c71dbce4

--- report/plato/files/app_scripts_controllers_about_js/report.history.js
+++ report/plato/files/app_scripts_controllers_about_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 07 Apr 2015 07:03:06 GMT","sloc":17,"lloc":3,"functions":1,"deliveredBugs":0.029974727502502494,"maintainability":91.96818238062446,"lintErrors":2,"difficulty":2.916666666666667}]
+__history = [{"date":"Tue, 07 Apr 2015 07:09:53 GMT","sloc":17,"lloc":3,"functions":1,"deliveredBugs":0.029974727502502494,"maintainability":91.96818238062446,"lintErrors":2,"difficulty":2.916666666666667}]
