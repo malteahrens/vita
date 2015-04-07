7a799ad9053d8f3ec543773dd13d5a3c238e959c
deploy.sh <xyz@test.de>
2015-04-07 08:13:19 +0000

    publish: test
    generated from commit cee2d88fc5a27bcf6b3f4f3054a5b97a1784def3

--- report/plato/files/test_karma_conf_js/report.history.js
+++ report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 07 Apr 2015 08:04:29 GMT","sloc":71,"lloc":14,"functions":1,"deliveredBugs":0.12997385661313562,"maintainability":61.71582235484889,"lintErrors":[],"difficulty":4.421052631578947}]
+__history = [{"date":"Tue, 07 Apr 2015 08:11:34 GMT","sloc":71,"lloc":14,"functions":1,"deliveredBugs":0.12997385661313562,"maintainability":61.71582235484889,"lintErrors":[],"difficulty":4.421052631578947}]
--- report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 07 Apr 2015 08:04:29 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Tue, 07 Apr 2015 08:11:34 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
