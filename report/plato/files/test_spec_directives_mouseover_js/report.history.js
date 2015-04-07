84b94f005c9b1a9c111348edcdebb232d3d0499f
deploy.sh <xyz@test.de>
2015-04-07 09:32:02 +0000

    publish: test if running on travis - if true, change the output path
    generated from commit a18a4368270bbd9b5b03c3f14df5ac4b3c48fd59

--- report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 07 Apr 2015 09:29:08 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Tue, 07 Apr 2015 09:30:20 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
