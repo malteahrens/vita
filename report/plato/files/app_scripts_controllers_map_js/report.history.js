e2d8067989c21dad1d04f95485e715b3b556a95e
deploy.sh <xyz@test.de>
2015-04-07 06:45:08 +0000

    publish: clipping
    generated from commit 8e7adf4d7e61755d165d1de7f205e54ddf0492df

--- report/plato/files/app_scripts_controllers_map_js/report.history.js
+++ report/plato/files/app_scripts_controllers_map_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 06 Apr 2015 19:46:48 GMT","sloc":382,"lloc":251,"functions":14,"deliveredBugs":4.401142369227029,"maintainability":52.774711100646655,"lintErrors":23,"difficulty":65.63526570048309}]
+__history = [{"date":"Tue, 07 Apr 2015 06:43:19 GMT","sloc":382,"lloc":251,"functions":14,"deliveredBugs":4.401142369227029,"maintainability":52.774711100646655,"lintErrors":23,"difficulty":65.63526570048309}]
--- report/plato/files/app_scripts_lib_mapbox_gl_js/report.history.js
+++ report/plato/files/app_scripts_lib_mapbox_gl_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 06 Apr 2015 19:47:00 GMT","sloc":20447,"lloc":12878,"functions":1339,"deliveredBugs":356.3179249230285,"maintainability":60.7675526734963,"lintErrors":51,"difficulty":403.66759564226}]
+__history = [{"date":"Tue, 07 Apr 2015 06:43:30 GMT","sloc":20447,"lloc":12878,"functions":1339,"deliveredBugs":356.3179249230285,"maintainability":60.7675526734963,"lintErrors":51,"difficulty":403.66759564226}]
