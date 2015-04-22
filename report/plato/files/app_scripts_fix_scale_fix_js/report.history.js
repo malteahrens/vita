0cfa41e6bfe9a80da28e691c0b1aabe023e55038
deploy.sh <xyz@test.de>
2015-04-22 18:12:14 +0000

    publish: test
    generated from commit 6348684513add4fb1d4d6b7e86503b2e61714f55

--- report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
+++ report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 22 Apr 2015 13:46:36 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
+__history = [{"date":"Wed, 22 Apr 2015 18:09:10 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
--- report/plato/files/app_scripts_lib_turf_min_js/report.history.js
+++ report/plato/files/app_scripts_lib_turf_min_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 22 Apr 2015 13:47:01 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
+__history = [{"date":"Wed, 22 Apr 2015 18:09:36 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
