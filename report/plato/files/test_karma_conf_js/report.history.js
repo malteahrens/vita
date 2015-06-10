09cb184f1f1c227895267eeda269f0dace04b800
deploy.sh <xyz@test.de>
2015-06-10 08:26:15 +0000

    publish: test
    generated from commit 8c16336598b1a754137f67ae0d4f95f50fb7d72e

--- report/plato/files/test_karma_conf_js/report.history.js
+++ report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 10 Jun 2015 08:11:56 GMT","sloc":71,"lloc":14,"functions":1,"deliveredBugs":0.12997385661313562,"maintainability":61.71582235484889,"lintErrors":[],"difficulty":4.421052631578947}]
+__history = [{"date":"Wed, 10 Jun 2015 08:23:52 GMT","sloc":71,"lloc":14,"functions":1,"deliveredBugs":0.12997385661313562,"maintainability":61.71582235484889,"lintErrors":[],"difficulty":4.421052631578947}]
