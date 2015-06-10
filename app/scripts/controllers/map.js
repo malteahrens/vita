adbef48dbcec9875966cd1219f2d2ba3bdc07bcb
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-10 09:31:04 +0200

    test

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -332,11 +332,14 @@ angular.module('angularApp')
                     $scope.heading = position.coords.heading;
                     try {
 
-                        var heading = position.coords.heading;
+                        // heading direction from sensor
+                        var heading = $scope.headingSensor;
+                        // heading direction from GPS
+                        //var heading = position.coords.heading;
                         if(heading > 180) {
                             heading = (heading - 180) * -1;
                         }
-                        var headingDirection = turf.destination(point, 0.1, $scope.headingSensor, "kilometers");
+                        var headingDirection = turf.destination(point, 0.1, heading, "kilometers");
                         var headingDirectionLine = turf.linestring([
                             location2,
                            headingDirection.geometry.coordinates
