c3c6dc0652dbd8b68971fdf777d8381ebb06e9b9
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-11 15:12:36 +0200

    test

--- deploy/scripts/github.sh
+++ deploy/scripts/github.sh
@@ -2,8 +2,8 @@
 set -o errexit #abort if any command fails
 
 #if no user identity is already set in the current git environment, use this:
-default_username=deploy.sh
-default_email=xyz@test.de
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 
 #repository to deploy to. must be readable and writable.
 repo=https://malteahrens:${GH_TOKEN}@github.com/malteahrens/vita
