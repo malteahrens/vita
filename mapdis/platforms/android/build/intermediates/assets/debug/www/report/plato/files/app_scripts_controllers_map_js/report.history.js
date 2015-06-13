49456981246097aa205a7b221b07891026059b43
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-13 15:23:10 +0200

    test

--- mapdis/platforms/android/build/intermediates/assets/debug/www/report/plato/files/app_scripts_controllers_map_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 10 Jun 2015 15:32:14 GMT","sloc":731,"lloc":492,"functions":26,"deliveredBugs":7.574732961926071,"maintainability":52.90419912722233,"lintErrors":39,"difficulty":79.00306748466258}]
--- mapdis/platforms/android/build/intermediates/assets/debug/www/report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 10 Jun 2015 15:32:56 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
