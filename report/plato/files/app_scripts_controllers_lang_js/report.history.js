0fb7ac9f9e6bb1983b615000aa1bf2c9d3abecc4
deploy.sh <xyz@test.de>
2015-06-13 23:19:52 +0000

    publish: tets
    generated from commit 948b3077179cecc44f8a70b17960568596f89d7d

--- report/plato/files/app_scripts_controllers_lang_js/report.history.js
+++ report/plato/files/app_scripts_controllers_lang_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 13 Jun 2015 23:05:06 GMT","sloc":25,"lloc":7,"functions":4,"deliveredBugs":0.09614709844115209,"maintainability":88.9679907446714,"lintErrors":4,"difficulty":4.909090909090909}]
+__history = [{"date":"Sat, 13 Jun 2015 23:16:58 GMT","sloc":25,"lloc":7,"functions":4,"deliveredBugs":0.09614709844115209,"maintainability":88.9679907446714,"lintErrors":4,"difficulty":4.909090909090909}]
