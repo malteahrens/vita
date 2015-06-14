b55c340b7c195b039e75ad2816d7240665af7202
deploy.sh <xyz@test.de>
2015-06-14 08:10:38 +0000

    publish: updated mapbox-gl-js
    generated from commit d0f77e65569e7ec3272cc577dcab761dd8e7cc17

--- report/plato/files/app_scripts_controllers_main_js/report.history.js
+++ report/plato/files/app_scripts_controllers_main_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 13 Jun 2015 23:16:58 GMT","sloc":28,"lloc":9,"functions":2,"deliveredBugs":0.08813484023772274,"maintainability":75.75840956065119,"lintErrors":3,"difficulty":5.5}]
+__history = [{"date":"Sun, 14 Jun 2015 08:07:49 GMT","sloc":28,"lloc":9,"functions":2,"deliveredBugs":0.08813484023772274,"maintainability":75.75840956065119,"lintErrors":3,"difficulty":5.5}]
--- report/plato/files/app_scripts_directives_mouseover_js/report.history.js
+++ report/plato/files/app_scripts_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 13 Jun 2015 23:16:59 GMT","sloc":69,"lloc":25,"functions":10,"deliveredBugs":0.27416722486195566,"maintainability":82.66298645354489,"lintErrors":5,"difficulty":10.365853658536587}]
+__history = [{"date":"Sun, 14 Jun 2015 08:07:50 GMT","sloc":69,"lloc":25,"functions":10,"deliveredBugs":0.27416722486195566,"maintainability":82.66298645354489,"lintErrors":5,"difficulty":10.365853658536587}]
