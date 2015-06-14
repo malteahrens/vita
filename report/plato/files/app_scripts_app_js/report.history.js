6b00acd19395f341fb44e4ea6d69d2f6db372081
deploy.sh <xyz@test.de>
2015-06-14 17:54:51 +0000

    publish: test
    generated from commit 4494b07db8730debd880a43f131dd231705c5b6d

--- report/plato/files/app_scripts_app_js/report.history.js
+++ report/plato/files/app_scripts_app_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 14 Jun 2015 17:14:55 GMT","sloc":64,"lloc":27,"functions":1,"deliveredBugs":0.2770180730309049,"maintainability":53.13389855711421,"lintErrors":2,"difficulty":5.830508474576271}]
+__history = [{"date":"Sun, 14 Jun 2015 17:50:58 GMT","sloc":64,"lloc":27,"functions":1,"deliveredBugs":0.2770180730309049,"maintainability":53.13389855711421,"lintErrors":2,"difficulty":5.830508474576271}]
