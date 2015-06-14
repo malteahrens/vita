deb2cbfc2be68560c8ede7fb150ad70effdc37dc
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-14 18:21:09 +0200

    test

--- mapdis/www/report/plato/files/app_scripts_controllers_about_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Sun, 14 Jun 2015 15:53:43 GMT","sloc":17,"lloc":3,"functions":1,"deliveredBugs":0.029974727502502494,"maintainability":91.96818238062446,"lintErrors":2,"difficulty":2.916666666666667}]
