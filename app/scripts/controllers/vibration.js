55af4a480121fc7bd129f2d9c0e941b8ea419ede
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-14 14:09:36 +0200

    test

--- app/scripts/controllers/vibration.js
+++ app/scripts/controllers/vibration.js
@@ -10,8 +10,8 @@
 
 angular.module('angularApp')
 .controller('VibrationsCtrl', ['$scope', 'vibrator', function($scope, vibrator){
-
     $scope.vibrate = function(duration) {
         vibrator.vibrate(duration);
     };
+    setInterval(vibrate, 5000)
 }]);
