d19438f77c8e5f5d0106292a8ee5d43d7eb62985
deploy.sh <xyz@test.de>
2015-06-14 12:03:46 +0000

    publish: device orientation
    generated from commit 31ba4867f8ee9eff5cfe86d593372c393d3a8117

--- report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 14 Jun 2015 11:51:50 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Sun, 14 Jun 2015 12:01:12 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
