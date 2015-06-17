c735dc7a82da4850588cfb568398751e6fda0d9b
deploy.sh <xyz@test.de>
2015-06-17 17:19:46 +0000

    publish: test
    generated from commit 886a6aa192239e87abd4b2a0d356985cd8e4a677

--- report/plato/files/app_scripts_controllers_vibration_js/report.history.js
+++ report/plato/files/app_scripts_controllers_vibration_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 16 Jun 2015 20:38:03 GMT","sloc":17,"lloc":5,"functions":2,"deliveredBugs":0.05186313713864835,"maintainability":87.41863596138215,"lintErrors":4,"difficulty":3.8333333333333335}]
+__history = [{"date":"Wed, 17 Jun 2015 17:16:25 GMT","sloc":17,"lloc":5,"functions":2,"deliveredBugs":0.05186313713864835,"maintainability":87.41863596138215,"lintErrors":4,"difficulty":3.8333333333333335}]
