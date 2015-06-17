a1d195a7a43fd7a12f06f599da01b311626faef0
blibla <blub@github.com>
2015-06-17 18:53:50 +0000

    Android App

--- mapdis/www/report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ mapdis/www/report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 17 Jun 2015 18:00:27 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Wed, 17 Jun 2015 18:50:09 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
