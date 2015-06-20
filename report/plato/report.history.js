5bb1fa05d708b27cba66d06c1681b75596ae967b
deploy.sh <xyz@test.de>
2015-06-20 17:17:13 +0000

    publish: test
    generated from commit 527b376505f3c9eb1c9f239f1f844983108f785b

--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 17:01:34 GMT","total":{"sloc":25637,"maintainability":1479.404348297755},"average":{"sloc":1281,"maintainability":"73.97"}}]
+__history = [{"date":"Sat, 20 Jun 2015 17:14:01 GMT","total":{"sloc":25645,"maintainability":1479.404348297755},"average":{"sloc":1282,"maintainability":"73.97"}}]
