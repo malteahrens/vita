a76469fa351e85e83ad409ecf0a000144f0d2add
blibla <blub@github.com>
2015-06-20 07:40:24 +0000

    Android App

--- mapdis/www/report/plato/files/app_scripts_controllers_old_js/report.history.js
+++ mapdis/www/report/plato/files/app_scripts_controllers_old_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 07:08:24 GMT","sloc":693,"lloc":471,"functions":25,"deliveredBugs":7.295464213036404,"maintainability":52.92464839856554,"lintErrors":38,"difficulty":79.06349206349206}]
+__history = [{"date":"Sat, 20 Jun 2015 07:35:53 GMT","sloc":693,"lloc":471,"functions":25,"deliveredBugs":7.295464213036404,"maintainability":52.92464839856554,"lintErrors":38,"difficulty":79.06349206349206}]
--- mapdis/www/report/plato/files/app_scripts_lib_javascript_util_js/report.history.js
+++ mapdis/www/report/plato/files/app_scripts_lib_javascript_util_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 07:08:25 GMT","sloc":15,"lloc":448,"functions":87,"deliveredBugs":7.987609720036332,"maintainability":67.45212671373943,"lintErrors":51,"difficulty":247.53658536585365}]
+__history = [{"date":"Sat, 20 Jun 2015 07:35:53 GMT","sloc":15,"lloc":448,"functions":87,"deliveredBugs":7.987609720036332,"maintainability":67.45212671373943,"lintErrors":51,"difficulty":247.53658536585365}]
