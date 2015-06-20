5c3ace02737f8b9c5f42dd87cbfa0a39c37f7d21
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-20 20:55:31 +0200

    test

--- mapdis/www/report/plato/files/app_scripts_lib_javascript_util_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Sat, 20 Jun 2015 18:07:05 GMT","sloc":15,"lloc":448,"functions":87,"deliveredBugs":7.987609720036332,"maintainability":67.45212671373943,"lintErrors":51,"difficulty":247.53658536585365}]
--- mapdis/www/report/plato/files/test_karma_conf_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Sat, 20 Jun 2015 18:07:30 GMT","sloc":72,"lloc":14,"functions":1,"deliveredBugs":0.13256548694536832,"maintainability":61.683191373752116,"lintErrors":[],"difficulty":4.397435897435898}]
