dd72200a4e61730bdf5ae7b47453b2ce3b2cf004
deploy.sh <xyz@test.de>
2015-06-20 16:19:11 +0000

    publish: test
    generated from commit dcee603db8a756db3b17ca3b934cc8899148e55d

--- report/plato/files/app_scripts_directives_mouseover_js/report.history.js
+++ report/plato/files/app_scripts_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 15:50:06 GMT","sloc":69,"lloc":25,"functions":10,"deliveredBugs":0.27416722486195566,"maintainability":82.66298645354489,"lintErrors":5,"difficulty":10.365853658536587}]
+__history = [{"date":"Sat, 20 Jun 2015 16:15:57 GMT","sloc":61,"lloc":24,"functions":10,"deliveredBugs":0.27090509710918675,"maintainability":82.66298645354489,"lintErrors":1,"difficulty":10.5}]
--- report/plato/files/app_scripts_lib_turf_min_js/report.history.js
+++ report/plato/files/app_scripts_lib_turf_min_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 15:50:42 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
+__history = [{"date":"Sat, 20 Jun 2015 16:16:20 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
