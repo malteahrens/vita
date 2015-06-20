cb2e71f3b5d7005262159d3feace152bca683963
Malte Ahrnes <MalteAhrens@arcor.de>
2015-06-20 08:59:12 +0200

    test

--- app/scripts/services/cordova.js
+++ app/scripts/services/cordova.js
@@ -7,17 +7,17 @@
  * If there is a value attached to the directive this will be the id for the mouseover action, otherwise it will try to read the id from the translation directive (if present)
  */
 angular.module('cordova', [])
-
-    .factory('deviceReady', function(){
-        console.log("factory loaded");
-        return function(done) {
-            console.log("inside factory");
-            var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
-            if ( app ) {
-               alert("running inside phonegap");
-            } else {
-                alert("running in browser...")
-                done();
-            }
-        };
-    });
+.factory('deviceReady', function(){
+    console.log("factory loaded");
+    return function(done) {
+        console.log("inside factory");
+        var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+        if ( app ) {
+           alert("running inside phonegap");
+            done();
+        } else {
+            alert("running in browser...")
+            done();
+        }
+    };
+});
