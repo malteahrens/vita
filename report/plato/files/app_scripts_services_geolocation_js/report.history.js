9a4f97b936e9a0f42e557df373cd90e63867f921
deploy.sh <xyz@test.de>
2015-06-20 15:54:12 +0000

    publish: test
    generated from commit 7f401403cc672ba8de836edfb65ce59029ec0399

--- report/plato/files/app_scripts_services_geolocation_js/report.history.js
+++ report/plato/files/app_scripts_services_geolocation_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 20 Jun 2015 15:36:07 GMT","sloc":26,"lloc":12,"functions":7,"deliveredBugs":0.12310279240667919,"maintainability":90.42404973317984,"lintErrors":5,"difficulty":7.269230769230769}]
+__history = [{"date":"Sat, 20 Jun 2015 15:50:42 GMT","sloc":26,"lloc":12,"functions":7,"deliveredBugs":0.12310279240667919,"maintainability":90.42404973317984,"lintErrors":5,"difficulty":7.269230769230769}]
