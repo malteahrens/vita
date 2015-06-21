608619c1f91a71a9cbe4c22dbfefcb42d7a2284d
blibla <blub@github.com>
2015-06-21 19:48:07 +0000

    Android App

--- mapdis/www/report/plato/files/app_scripts_app_js/report.history.js
+++ mapdis/www/report/plato/files/app_scripts_app_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 19:26:08 GMT","sloc":67,"lloc":27,"functions":1,"deliveredBugs":0.2850644746496479,"maintainability":53.13389855711421,"lintErrors":2,"difficulty":5.836065573770492}]
+__history = [{"date":"Sun, 21 Jun 2015 19:42:36 GMT","sloc":55,"lloc":21,"functions":1,"deliveredBugs":0.2089346554928834,"maintainability":56.60036598527072,"lintErrors":2,"difficulty":5.6}]
