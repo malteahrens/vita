a660bf2d751860c86c955b55bafa2b439224cb72
deploy.sh <xyz@test.de>
2015-06-21 18:15:52 +0000

    publish: added cordova.js
    generated from commit 215ea2a4086e8d9871d5b32a0412df29807b7faa

---
+++ report/plato/files/app_scripts_lib_cordova_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 21 Jun 2015 18:11:32 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
--- report/plato/files/app_scripts_lib_turf_min_js/report.history.js
+++ report/plato/files/app_scripts_lib_turf_min_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 17:25:45 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
+__history = [{"date":"Sun, 21 Jun 2015 18:12:03 GMT","sloc":18,"lloc":12727,"functions":2911,"deliveredBugs":631.2936131434615,"maintainability":67.5646963176419,"lintErrors":51,"difficulty":786.2776560788609}]
