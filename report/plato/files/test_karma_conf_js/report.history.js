e01770b85e324989989408ba9e35a6282986de72
deploy.sh <xyz@test.de>
2015-06-21 20:01:05 +0000

    publish: test
    generated from commit 013d5aa87f9e886841b1eb6bfb2c43926581d093

--- report/plato/files/test_karma_conf_js/report.history.js
+++ report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 19:43:01 GMT","sloc":73,"lloc":14,"functions":1,"deliveredBugs":0.13516166205748917,"maintainability":61.65088817139504,"lintErrors":[],"difficulty":4.375}]
+__history = [{"date":"Sun, 21 Jun 2015 19:57:38 GMT","sloc":73,"lloc":14,"functions":1,"deliveredBugs":0.13516166205748917,"maintainability":61.65088817139504,"lintErrors":[],"difficulty":4.375}]
--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 19:43:01 GMT","total":{"sloc":25878,"maintainability":1634.6321529754152},"average":{"sloc":1176,"maintainability":"74.30"}}]
+__history = [{"date":"Sun, 21 Jun 2015 19:57:38 GMT","total":{"sloc":25878,"maintainability":1633.7870497136257},"average":{"sloc":1176,"maintainability":"74.26"}}]
