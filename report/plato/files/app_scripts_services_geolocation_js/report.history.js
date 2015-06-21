97ba7a4003c92b3db93d2aafd77e455a6305a549
deploy.sh <xyz@test.de>
2015-06-21 06:43:51 +0000

    publish: test
    generated from commit a4b2f7116dcdf71e480fb0c797ef3a784c44b145

--- report/plato/files/app_scripts_services_geolocation_js/report.history.js
+++ report/plato/files/app_scripts_services_geolocation_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 06:22:52 GMT","sloc":21,"lloc":13,"functions":7,"deliveredBugs":0.13023633414072375,"maintainability":89.26450678327737,"lintErrors":8,"difficulty":7.071428571428571}]
+__history = [{"date":"Sun, 21 Jun 2015 06:40:24 GMT","sloc":21,"lloc":13,"functions":7,"deliveredBugs":0.13023633414072375,"maintainability":89.26450678327737,"lintErrors":8,"difficulty":7.071428571428571}]
--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 21 Jun 2015 06:22:52 GMT","total":{"sloc":25647,"maintainability":1480.6582692956804},"average":{"sloc":1282,"maintainability":"74.03"}}]
+__history = [{"date":"Sun, 21 Jun 2015 06:40:24 GMT","total":{"sloc":25647,"maintainability":1480.6582692956804},"average":{"sloc":1282,"maintainability":"74.03"}}]
